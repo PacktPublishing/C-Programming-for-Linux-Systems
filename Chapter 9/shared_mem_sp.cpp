359febf989aac7093c6b7a1b163580ad5bb760e3
Desislav Andreev <desislav.andreev@gmail.com>
2023-07-30 01:19:10 +0300

    Fixing string_view misusage

--- Chapter 9/shared_mem_sp.cpp
+++ Chapter 9/shared_mem_sp.cpp
@@ -12,9 +12,10 @@
 using namespace std;
 
 constexpr auto PROT_RW = PROT_READ | PROT_WRITE;
-constexpr auto SHM_ID  = "/test_shm";
+constexpr auto SHM_ID = "/test_shm";
+constexpr auto BUF_SIZE = 256;
 
-string_view message = "This is a testing message!";
+string_view message{ "This is a testing message!" };
 
 mutex _mutex;
 condition_variable cond_var;
@@ -23,48 +24,48 @@ bool work_done = false;
 template<typename T>
 struct mmap_deallocator {
     size_t m_size;
-    mmap_deallocator(size_t size) : m_size{size} {}
+    mmap_deallocator(size_t size) : m_size{ size } {}
 
-    void operator()(T *ptr) const {
-       munmap(ptr, m_size);
+    void operator()(T* ptr) const {
+        munmap(ptr, m_size);
     }
 };
 
-template<typename T, typename... Args>
-auto producer(Args&&... args) {
+template<typename T, typename N>
+auto producer(T buffer, N size) {
     unique_lock<mutex> lock(_mutex);
-    cond_var.wait(lock, []{ return work_done == false; });
-    if (int fd = shm_open(SHM_ID, O_CREAT | O_RDWR, 0644); 
-       fd != -1) {
-        ftruncate(fd, sizeof(T));
-        if (auto ptr = mmap(0, sizeof(T), 
-                            PROT_RW, MAP_SHARED, 
-                            fd, 0)) {
-            auto obj = new (ptr) T(args...);
-            auto del = mmap_deallocator<T>(sizeof(T));
+    cond_var.wait(lock, [] { return work_done == false; });
+    if (int fd = shm_open(SHM_ID, O_CREAT | O_RDWR, 0644);
+        fd != -1) {
+        ftruncate(fd, size);
+        if (auto ptr = mmap(0, size,
+            PROT_RW, MAP_SHARED,
+            fd, 0)) {
+            auto obj = new (ptr) T(buffer);
+            auto del = mmap_deallocator<T>(size);
             work_done = true;
             cond_var.notify_one();
-            return unique_ptr<T, 
+            return unique_ptr<T,
                 mmap_deallocator<T>>(obj, del);
         }
     }
     throw bad_alloc();
 }
 
-template<typename T>
-auto consumer() {
+template<typename T, typename N>
+auto consumer(N size) {
     unique_lock<mutex> lock(_mutex);
-    cond_var.wait(lock, []{ return work_done == true; });
-    if (int fd = shm_open(SHM_ID, O_RDWR, 0644); 
-       fd != -1) {
-        ftruncate(fd, sizeof(T));
-        if (auto ptr = mmap(0, sizeof(T), 
-                            PROT_RW, MAP_SHARED, 
-                            fd, 0)) {
+    cond_var.wait(lock, [] { return work_done == true; });
+    if (int fd = shm_open(SHM_ID, O_RDWR, 0644);
+        fd != -1) {
+        ftruncate(fd, size);
+        if (auto ptr = mmap(0, size,
+            PROT_RW, MAP_SHARED,
+            fd, 0)) {
             auto obj = static_cast<T*>(ptr);
             auto del = mmap_deallocator<T>(sizeof(T));
             work_done = false;
-            return unique_ptr<T, 
+            return unique_ptr<T,
                 mmap_deallocator<T>>(obj, del);
         }
     }
@@ -72,16 +73,17 @@ auto consumer() {
 }
 
 int main() {
-    thread server([]{ 
-        auto ptr = 
-            producer<string_view>(message);
-            cout << "Sending: " << *ptr << '\n';
-    });
-    thread client([]{ 
-        auto ptr = 
-            consumer<string_view>();
-            cout << "Receiving: " << *ptr<< '\n'; 
-    });
+
+    thread server([] {
+        auto ptr =
+            producer<const char*, size_t>(message.data(), message.size());
+        cout << "Sending: " << *ptr << '\n';
+        });
+    thread client([] {
+        auto ptr =
+            consumer<char*, size_t>(message.size());
+        cout << "Receiving: " << *ptr << '\n';
+        });
 
     server.join();
     client.join();

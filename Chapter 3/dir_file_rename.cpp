bf955187f61307fd589f295b3ba91338f7d40696
Desislav Andreev <desislav.andreev@gmail.com>
2023-08-24 10:17:26 +0300

    Updates after review

--- Chapter 3/dir_file_rename.cpp
+++ Chapter 3/dir_file_rename.cpp
@@ -6,7 +6,7 @@ using namespace std::filesystem;
 
 int main() {
     if (exists("some_data_sl")) {
-        rename("some_data_sl", "some_data_sl_rndm")
+        rename("some_data_sl", "some_data_sl_rndm");
     }
     return 0;
 }

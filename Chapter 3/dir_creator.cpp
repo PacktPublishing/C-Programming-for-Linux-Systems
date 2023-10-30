c65ca7cf32085ac22c8e78da492da0fd2017256e
Desislav Andreev <deandreev@deandreevEMD6R.vmware.com>
2023-10-30 15:40:13 +0200

    Fixing the scope of the else clause for dir_creator

--- Chapter 3/dir_creator.cpp
+++ Chapter 3/dir_creator.cpp
@@ -9,8 +9,10 @@ int main() {
     auto result = create_directory("test_dir", ec);
     if (result)
         cout << "Directory created successfuly!\n";
-    else
+    else {
         cout << "Directory creation failed!\n";
         cout << ec.category().name() << endl;
+    }
+
     return 0;
 }
